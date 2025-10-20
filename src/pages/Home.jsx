5222fa80f9ada945f313e6f941a33a16f3202579
Kennedy Ramos <devnedy@gmail.com>
2025-10-20 17:20:42 -0300

    footer component creation by adding images

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,14 @@
 //COMPONENTS
 import Header from "../components/Header/Header"
+import Footer from "../components/Footer/Footer"
 
 export default function Home(){
     return(
         <>
             <Header></Header>
             <h1>Home</h1>
+
+            <Footer></Footer>
         </>
     )
 }
