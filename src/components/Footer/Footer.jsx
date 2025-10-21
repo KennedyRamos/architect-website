55ad5cb9631e11f1a8914d4f884ec235f0016fa0
Kennedy Ramos <devnedy@gmail.com>
2025-10-21 16:12:12 -0300

    finishing the footer, creating the component css and rearranging the layout

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom';
 
 // ASSETS
 import './Footer.css'
-import Logo from '../../assets/logo.svg';
+import LogoName from '../../assets/logo-with-name.svg';
 import BrazilIcon from '../../assets/brazil-icon.svg';
 import UsaIcon from '../../assets/usa-icon.svg';
 import FacebookIcon from '../../assets/facebook-icon.svg';
@@ -16,8 +16,8 @@ export default function Footer(){
                 
                 <div className='d-flex jc-between'>
                     
-                    <div className='footerlogo-col'>
-                        <img src={Logo} />
+                    <div className='footer-logo-col'>
+                        <img src={LogoName} className='footer-logo'/>
                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis eveniet veniam nulla deleniti a culpa!</p>
                         
                         <div className='d-flex jc-between'>
@@ -30,12 +30,36 @@ export default function Footer(){
                             <a target='_blank' href="#">
                                 <img src={LinkedinIcon} />
                             </a>
-                        </div>
-
+                        </div>     
                     </div>
-                
+
+                    <div className='d-flex'>
+                        <div className='footer-col'>
+                            <h3>Pages</h3>
+                            <ul>
+                                <li><Link to="/">Home</Link></li>
+                                <li><Link to="/About">About</Link></li>
+                                <li><Link to="/Projects">Projects</Link></li>
+                                <li><Link to="/Contact">Contact</Link></li>
+                            </ul>
+                        </div>
+                        <div className='footer-col'>
+                            <h3>Contact</h3>
+                            <p>Jardim Jordão - Jaboatão dos guararapes |Pernambuco</p>
+                            <p>devnedy@gmail.com</p>
+                            <p>(81) 97916-1344</p>
+                        </div>
+                    </div>
                 </div>
 
+                <div className='d-flex jc-between footer-copy'>
+                    <p>Copyright © Dev Kennedy Ramos - 2025</p>
+                    <div className='langs-area d-flex'>
+                        <img src={BrazilIcon} />
+                        <img src={UsaIcon} />
+                    </div>
+                </div>
+                
             </div>
         </footer>
     )
