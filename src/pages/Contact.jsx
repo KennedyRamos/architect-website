e724381b568b61fefd89e4ad251bd6edd7f18288
Kennedy Ramos <devnedy@gmail.com>
2025-10-26 15:18:47 -0300

    Organizing the page layout, marking the form

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,7 +1,22 @@
-export default function Contact(){
+//COMPONENTS
+import ContactForm from "../components/ContactForm/ContactForm";
+import Banner from "../components/Banner/Banner";
+import Header from "../components/Header/Header";
+import Footer from "../components/Footer/Footer";
+
+export default function Home(){
     return(
         <>
-            <h1>Contact</h1>
+            <Header></Header>
+            <Banner title="Contact" image="bg-contact.jpg"/>
+            
+            
+            <div className="container">
+                <ContactForm />
+            </div>
+
+
+            <Footer></Footer>
         </>
     )
 }
