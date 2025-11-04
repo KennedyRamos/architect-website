ccb1ab216da94c3a243d9eede427a60bc87c217b
Kennedy Ramos <devnedy@gmail.com>
2025-11-04 10:52:59 -0300

    creation and styling of contact page form

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,10 +1,30 @@
+import { useState, useEffect } from 'react';
 import './ContactForm.css';
+
+// COMPONENTS
+import Button from '../Button/Button';
 
 export default function ContactForm(){
     return(
         <div className='contact-form d-flex fd-column al-center'>
             <h2>We love meeting new people and helping them.</h2>
-            <form>WIP...</form>
+            <form>
+                <div className='d-flex form-group'>
+                    <input className='form-input' type="text" name="nome" id="nome" placeholder='Name *' />
+                    
+                    <input className='form-input' type="email" name="email" id="email" placeholder='Email *' />
+                    
+                </div>
+                <div className='d-flex form-group'>
+                    <textarea className='form-input' name="message" id="message" placeholder='Mensagem *' rows="4"></textarea>
+                </div>
+
+                <div className='d-flex al-center jc-end form-group'>
+                    
+                    <Button type="submit" buttonStyle="secondary">Enviar</Button>
+                    
+                </div>
+            </form>
         </div>
         
     )
