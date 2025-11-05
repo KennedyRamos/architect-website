684600586149577de107c74199221f1247adba1b
Kennedy Ramos <devnedy@gmail.com>
2025-11-05 20:07:24 -0300

    adding context to the about page banner

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,14 +1,21 @@
+import { useContext } from 'react';
+
 //COMPONENTS
 import AboutText from "../components/AboutText/AboutText";
 import Banner from "../components/Banner/Banner";
 import Header from "../components/Header/Header";
 import Footer from "../components/Footer/Footer";
 
+// CONTEXT
+import { AppContext } from '../contexts/AppContext';
+
 export default function Home(){
+    const appContext = useContext(AppContext)
+
     return(
         <>
             <Header></Header>
-            <Banner title="About us" image="bg-abaut.jpg"/>
+            <Banner title={appContext.languages[appContext.language].menu.about} image="bg-abaut.jpg"/>
             
             
             <div className="container">
