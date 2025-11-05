4230330065bbf236ed343dfdfb215cf493ff8bf4
Kennedy Ramos <devnedy@gmail.com>
2025-11-05 20:20:34 -0300

    adding context to the projects and contact page banner

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,14 +1,23 @@
+import { useContext } from 'react';
+
+
 //COMPONENTS
 import Banner from "../components/Banner/Banner";
 import ProjectsList from "../components/ProjectsList/ProjectsList";
 import Header from "../components/Header/Header";
 import Footer from "../components/Footer/Footer";
 
+// CONTEXT
+import { AppContext } from '../contexts/AppContext';
+
 export default function Home(){
+    
+    const appContext = useContext(AppContext)
+    
     return(
         <>
             <Header></Header>
-            <Banner title="Projects" image="bg-projects.jpg"/>
+            <Banner title={appContext.languages[appContext.language].menu.projects} image="bg-projects.jpg"/>
             
             
             <div className="container">
